a6e33bbb175f81898924e32a6bb5056eadf1f119
J.E.Prog <belemejohannaeunice@gmail.com>
2024-01-04 21:35:37 +0100

    myFirstCommit

---
+++ login_page_typescript/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import LoginForm from './LoginForm';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Login Form</h1>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default App;
---
+++ login_page_typescript/my-app/src/LoginForm.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const LoginForm: React.FC = () => {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('username:', username);
+    console.log('password:', password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
